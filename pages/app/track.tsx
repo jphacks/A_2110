eeab18fe20eba0dee7dce5fe0613163fcbcfffff
tofu4956 <tofu4956@users.noreply.github.com>
2021-10-26 07:22:43 +0900

    add signin system PoC (NextAuth.js)

--- pages/app/track.tsx
+++ pages/app/track.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 
 const Track: NextPage = () => {
+    const { data: session, status } = useSession();
+    const loading = status === 'loading'
     return (
         <div>
             <p>aaa</p>
