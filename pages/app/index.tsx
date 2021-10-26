78ff40dc345b2775871267ccc31fee6ddae6b583
tofu4956 <tofu4956@users.noreply.github.com>
2021-10-27 06:55:30 +0900

    add DeviceMotionHandler

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, VFC, FC } from 'react'
 import { useSession } from 'next-auth/react'
 import { NextPage } from 'next'
 import Header from '../../components/header'
-const AppHome = () => {
+const AppHome = () : JSX.Element => {
   const { data: session, status } = useSession()
   const loading = status === 'loading'
   const [ content , setContent ] = useState()
--- pages/app/track.tsx
+++ pages/app/track.tsx
@@ -1,14 +1,43 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { DependencyList, useCallback, useEffect } from "react";
+import Header from "../../components/header";
+//Windowevent取得用Hooks
+const useWindowEvent = <K extends keyof WindowEventMap>(
+  type: K,
+  listener: (this: Window, ev: WindowEventMap[K]) => any,
+  deps: DependencyList,
+  options?: boolean | AddEventListenerOptions
+) => useEffect(() => {
+    if(window){
+      window.addEventListener(type, listener, options);
+      return () => {
+        window.removeEventListener(type,listener, options);
+      }
+    }
+  }, deps);
+
 
 const Track: NextPage = () => {
-    const { data: session, status } = useSession();
-    const loading = status === 'loading'
-    return (
-        <div>
-            <p>aaa</p>
-        </div>
-    )
+  const { data: session, status } = useSession();
+  const loading = status === 'loading';
+  //加速度取得ハンドラ
+  const MotionHandler = useCallback(
+    (e: DeviceMotionEvent) => {
+      console.log(e.accelerationIncludingGravity?.x);
+      console.log(e.accelerationIncludingGravity?.y);
+      console.log(e.accelerationIncludingGravity?.z);
+      e.preventDefault();
+    }, []
+  );
+  //加速度取得
+  useWindowEvent("devicemotion", MotionHandler, []);
+  return (
+    <div>
+      <Header/>
+      <p>aaa</p>
+    </div>
+  )
 }
 
 export default Track;
