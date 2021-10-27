ce3ca0c9a3109718210af9dfd33684b72e5ff934
tofu4956 <tofu4956@users.noreply.github.com>
2021-10-28 03:45:13 +0900

    add restricts of timer

--- pages/app/track.tsx
+++ pages/app/track.tsx
@@ -6,77 +6,69 @@ import { DependencyList, useCallback, useEffect, useState } from "react";
 import Button from '@mui/material/Button';
 import Header from "../../components/header";
 import Link from "next/link";
-//Windowevent取得用Hooks
 type Motion = {
-  x: Number,
-  y: Number,
-  z: Number,
-}
-
-const useWindowEvent = <K extends keyof WindowEventMap>(
-  type: K,
-  listener: (this: Window, ev: WindowEventMap[K]) => any,
-  deps: DependencyList,
-  options?: boolean | AddEventListenerOptions
-) => useEffect(() => {
-    if(window){
-      window.addEventListener(type, listener, options);
-      return () => {
-        window.removeEventListener(type,listener, options);
-      }
-    }
-  }, deps);
-
-let Motionbuffer : Motion = {
-  x: 0,
-  y: 0,
-  z: 0,
+  x: number,
+  y: number,
+  z: number,
 }
 
 function NumberTypeAdapter(x: null | undefined | number) : number{
   return x = x !== (null) ? (x !== (undefined) ? x : 0) : 0
 }
 //加速度取得ハンドラ
-const UpdaterElement = () => {
-  return(
-  <section>
-    <p>x: {Motionbuffer.x}</p>
-    <p>y: {Motionbuffer.y}</p>
-    <p>z: {Motionbuffer.z}</p>
-  </section>
-  );
-}
-//運動時間計測
-const Stopwatch = () => {
-  const { seconds, minutes, hours, days, isRunning, start, pause, reset } =
-    useStopwatch({ autoStart: false });
-
-  return (
-    <div style={{ textAlign: "center" }}>
-      <div style={{ fontSize: "50px" }}>
-        <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
-      </div>
-      <p>{isRunning ? "Running" : "Not running"}</p>
-      <Button onClick={start}>Start</Button>
-      <Button onClick={pause}>Pause</Button>
-    </div>
-  );
-}
-
-//現在位置取得
-
-//運動距離計測(Google Maps Direction API)
 
 
 //ページ実装
 const Track: NextPage = () => {
+  const { seconds, minutes, hours, days, isRunning, start, pause, reset } =
+  useStopwatch({ autoStart: false });
   const { data: session, status } = useSession();
   const loading = status === 'loading';
   const [speedX, setSpeedX] = useState<number>(0);
   const [speedY, setSpeedY] = useState<number>(0);
   const [speedZ, setSpeedZ] = useState<number>(0);
+  let counter = 0;
+  let acclerationAverageArray: Motion[] = [];
+  let acclerationAverage: Motion = {
+    x: 0,
+    y: 0,
+    z: 0,
+  }
+  //加速度平均算出
+  function getAcclerationAverage(acc: DeviceMotionEventAcceleration){
+    counter++;
+    let acclerationBuffer : Motion = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+    acclerationBuffer.x = NumberTypeAdapter(acc.x);
+    acclerationBuffer.y = NumberTypeAdapter(acc.y);
+    acclerationBuffer.z = NumberTypeAdapter(acc.z);
+    if(counter !== 180){
+      acclerationAverageArray.push(acclerationBuffer);
+      counter++;
+      acclerationAverage.x = (((acclerationAverage.x *(counter-1)) + (acclerationBuffer.x))/counter);
+      acclerationAverage.y = (((acclerationAverage.y *(counter-1))+ (acclerationBuffer.y))/counter);
+      acclerationAverage.z = (((acclerationAverage.z *(counter-1))+ (acclerationBuffer.z))/counter);
+    }else if(counter >= 180){
+      acclerationAverageArray.push(acclerationBuffer);
+      let subjectdata: Motion | undefined = acclerationAverageArray.shift();
+      
+      if(subjectdata !== (undefined)){
+        //180秒前のデータ削除
+        acclerationAverage.x = (((acclerationAverage.x*(counter)) - (NumberTypeAdapter(subjectdata.x)))/counter);
+        acclerationAverage.y = (((acclerationAverage.y*(counter)) - (NumberTypeAdapter(subjectdata.y)))/counter);
+        acclerationAverage.z = (((acclerationAverage.z*(counter))- (NumberTypeAdapter(subjectdata.z)))/counter);
+        //新しいデータを挿入
+        acclerationAverage.x = (((acclerationAverage.x *(counter-1)) + (acclerationBuffer.x))/counter);
+        acclerationAverage.y = (((acclerationAverage.y *(counter-1))+ (acclerationBuffer.y))/counter);
+        acclerationAverage.z = (((acclerationAverage.z *(counter-1))+ (acclerationBuffer.z))/counter);
+      }
+    }
+  }
+  // Android or other ios
   useEffect(() => {
-    // Android or other ios
     window.addEventListener("devicemotion", (e: DeviceMotionEvent) => {
       if (!e.accelerationIncludingGravity) {
         alert('event.accelerationIncludingGravity is null');
@@ -85,11 +77,15 @@ const Track: NextPage = () => {
       setSpeedX(NumberTypeAdapter(e.accelerationIncludingGravity.x));
       setSpeedY(NumberTypeAdapter(e.accelerationIncludingGravity.y));
       setSpeedZ(NumberTypeAdapter(e.accelerationIncludingGravity.z));
+      setInterval(getAcclerationAverage,1000, e.accelerationIncludingGravity);
+      if(counter >= 180 && (Math.abs(acclerationAverage.x - NumberTypeAdapter(e.accelerationIncludingGravity.x)) < 2 || Math.abs(acclerationAverage.y - NumberTypeAdapter(e.accelerationIncludingGravity.y)) < 2 || Math.abs(acclerationAverage.z - NumberTypeAdapter(e.accelerationIncludingGravity.z)) < 2)){
+        pause;
+      }
     })
   })
+  // only have ios 13 above
   const deviceMotionRequest = () => {
     //@ts-ignore
-    // only have ios 13 above
     if(DeviceMotionEvent.requestPermission){
       //@ts-ignore
       DeviceMotionEvent.requestPermission()
@@ -104,6 +100,10 @@ const Track: NextPage = () => {
               setSpeedX(NumberTypeAdapter(e.accelerationIncludingGravity.x));
               setSpeedY(NumberTypeAdapter(e.accelerationIncludingGravity.y));
               setSpeedZ(NumberTypeAdapter(e.accelerationIncludingGravity.z));
+              setInterval(getAcclerationAverage,1000);
+              if(counter >= 180 && (Math.abs(acclerationAverage.x - NumberTypeAdapter(e.accelerationIncludingGravity.x)) < 2 || Math.abs(acclerationAverage.y - NumberTypeAdapter(e.accelerationIncludingGravity.y)) < 2 || Math.abs(acclerationAverage.z - NumberTypeAdapter(e.accelerationIncludingGravity.z)) < 2)){
+                pause;
+              }
             })
           }
         })
@@ -113,16 +113,26 @@ const Track: NextPage = () => {
       }
     }
   return (
-    <div>
-        <Header/>
+    <>
+    <Header/>
+    <div style={{textAlign: "center"}}>
         <Button variant="outlined" onClick={deviceMotionRequest}>ios13の場合は最初にここをタップ</Button>
-        <Stopwatch/>
+        <div style={{ textAlign: "center" }}>
+          <div style={{ fontSize: "50px" }}>
+            <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+          </div>
+            <p>{isRunning ? "Running" : "Not running"}</p>
+            <Button onClick={start}>Start</Button>
+            <Button onClick={pause}>Pause</Button>
+          </div>
+        <Button variant="contained">完了する！</Button>
         <div>
           <span>x: {speedX * 100} </span>
           <span>y: {speedY * 100} </span>
           <span>z: {speedZ * 100} </span>
         </div>
     </div>
+    </>
   )
 }
 
